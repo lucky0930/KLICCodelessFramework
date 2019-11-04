8280f26cd4e7433eddb24ccb77f14e4c9b11d0cc
Rupeshkv <venkatrupesh@gmail.com>
2019-11-04 08:46:58 +0000

    Submission Page

---
+++ src/test/java/com/test/automation/repository/SubmissionSummaryPage.java
@@ -0,0 +1,17 @@
+package com.test.automation.repository;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.test.automation.common.Page;
+import com.test.automation.common.SeHelper;
+public class SubmissionSummaryPage extends Page{
+
+	public static By Back = By.xpath("//*[@ng-click='navgateToSource()']");
+
+	public static WebElement Back(SeHelper se) {
+
+		return se.element().getElement(Back);
+	}
+}
+
