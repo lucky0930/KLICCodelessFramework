8f0b38018da36e7e603bb2b913aa6f18ae9d7826
Rupeshkv <venkatrupesh@gmail.com>
2019-11-04 09:37:12 +0000

    Updating for Canonital path

--- src/test/java/com/test/automation/common/SystemPropertyUtil.java
+++ src/test/java/com/test/automation/common/SystemPropertyUtil.java
@@ -1,48 +1,61 @@
 package com.test.automation.common;
 
 import java.net.Socket;
+import java.io.File;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
 public class SystemPropertyUtil {
-	
-		
+
 	private final static String windowWidthKey = "window.width";
 	private final static String windowHeightKey = "window.height";
 	private final static String testDataDirectorykey = "test.data.directory";
 	private final static String httpCredentialsKey = "http.credentials";
-	private final static String browsersKey = "browsers";	
+	private final static String browsersKey = "browsers";
 	private final static String baseUrlKey = "base.url";
 	private final static String localeUrlKey = "locale.url";
-	//private final static String baseUrlDefault = "http://138.91.124.246:1050/pages/login.html";
+	// private final static String baseUrlDefault =
+	// "http://138.91.124.246:1050/pages/login.html";
 	private final static String baseUrlDefault = "https://vam-bd-agentuw-qa-wapp.azurewebsites.net";
 	private final static String localeUrlDefault = "";
-	
-	//Default values
+
+	// Default values
 	private final static int windowWidthDefault = 1920;
 	private final static int windowHeightDefault = 1080;
 	private final static String testDataDirectoryDefault = "target/test-classes";
 	private final static String httpCredentialsDefault = "true";
 	private final static String browsersDefault = "Chrome";
 
-	
-	private final static int windowWidth = System.getProperties().containsKey(windowWidthKey) ? Integer.parseInt(System.getProperty(windowWidthKey)) : windowWidthDefault;		
-	private final static int windowHeight =System.getProperties().containsKey(windowHeightKey) ? Integer.parseInt(System.getProperty(windowHeightKey)) : windowHeightDefault;
-	private final static String browsers =			
-			System.getProperties().containsKey(browsersKey) ? System.getProperty(browsersKey) : browsersDefault; //new enums
-	
-	private final static String testDataDirectory = System.getProperties().containsKey(testDataDirectorykey) ? System.getProperty(testDataDirectorykey) : testDataDirectoryDefault;
-	private final static boolean httpCredentials = Boolean.parseBoolean(System.getProperties().containsKey(httpCredentialsKey) ? System.getProperty(httpCredentialsKey) : httpCredentialsDefault);
+	private final static int windowWidth = System.getProperties().containsKey(windowWidthKey)
+			? Integer.parseInt(System.getProperty(windowWidthKey))
+			: windowWidthDefault;
+	private final static int windowHeight = System.getProperties().containsKey(windowHeightKey)
+			? Integer.parseInt(System.getProperty(windowHeightKey))
+			: windowHeightDefault;
+	private final static String browsers = System.getProperties().containsKey(browsersKey)
+			? System.getProperty(browsersKey)
+			: browsersDefault; // new enums
 
-	
-	private final static String baseUrl = System.getProperties().containsKey(baseUrlKey) ? System.getProperty(baseUrlKey) : baseUrlDefault;
-	//private final static String baseUrl = "google.co.in";
-	private final static String localeUrl = System.getProperties().containsKey(localeUrlKey) ? System.getProperty(localeUrlKey) : localeUrlDefault;
-	
+	private final static String testDataDirectory = System.getProperties().containsKey(testDataDirectorykey)
+			? System.getProperty(testDataDirectorykey)
+			: testDataDirectoryDefault;
+	private final static boolean httpCredentials = Boolean.parseBoolean(
+			System.getProperties().containsKey(httpCredentialsKey) ? System.getProperty(httpCredentialsKey)
+					: httpCredentialsDefault);
+
+	private final static String baseUrl = System.getProperties().containsKey(baseUrlKey)
+			? System.getProperty(baseUrlKey)
+			: baseUrlDefault;
+	// private final static String baseUrl = "google.co.in";
+	private final static String localeUrl = System.getProperties().containsKey(localeUrlKey)
+			? System.getProperty(localeUrlKey)
+			: localeUrlDefault;
+	private static String testDataPath = "\\resources\\test_data\\VM_TestData_Sample1.xlsx";
+
 	public static String getBaseUrl() {
 		return baseUrl;
 	}
-	
+
 	public static String getLocale() {
 		return localeUrl;
 	}
@@ -50,29 +63,42 @@ public class SystemPropertyUtil {
 	public static int getWindowWidth() {
 		return windowWidth;
 	}
-	
+
 	public static int getWindowHeight() {
 		return windowHeight;
 	}
-	
+
 	public static String getTestDataDirectory() {
 		return testDataDirectory;
 	}
-	
+
 	public static boolean getHttpCredentials() {
 		return httpCredentials;
 	}
-	
-	
+
 	public static String getBrowsers() {
-		//translate old style to new SeHelper enums		
+		// translate old style to new SeHelper enums
 		return browsers;
 	}
-	
-	public static String getBaseStoreUrl()
-	{
+
+	public static String getBaseStoreUrl() {
 		return getBaseUrl();
 	}
-	
+
+	public static String getRootPath() throws IOException {
+		return new File(".").getCanonicalPath();
+	}
+
+	public static String getTestDataSheetPath() {
+		
+
+		try {
+			return getRootPath() + testDataPath;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
--- src/test/java/com/test/automation/common/Utils/TestUtil.java
+++ src/test/java/com/test/automation/common/Utils/TestUtil.java
@@ -1,49 +1,17 @@
 package com.test.automation.common.Utils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.TreeMap;
 
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.ss.util.NumberToTextConverter;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.apache.poi.EncryptedDocumentException;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-
-import java.io.IOException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-import com.fasterxml.jackson.annotation.JsonFormat.Value;
-import com.secura.ap.pages.StartupPage;
 import com.test.automation.common.SeHelper;
 import com.test.automation.common.SystemPropertyUtil;
 import com.test.automation.common.framework.XlsData;
-
-import jxl.Workbook;
 
 public class TestUtil {
 
 	static XlsData objXlsData = new XlsData();
 
-	static String TESTDATA_SHEET_PATH = "C:\\Users\\Evan.Verbus\\git\\VMSF\\VMSeleniumFramework\\resources\\test_data\\VM_TestData_Sample1.xlsx";
+	static String TESTDATA_SHEET_PATH = SystemPropertyUtil.getTestDataSheetPath();
 
 	ExcelReader excelReader = new ExcelReader();
 	List<String> sheetCollection = excelReader.sheetCollection;
@@ -78,7 +46,7 @@ public class TestUtil {
 			e.printStackTrace();
 		}
 		LinkedHashMap<String, LinkedHashMap<String, String>> tableData = excelReader.GetTestData(TestCaseNumber,
-				TESTDATA_SHEET_PATH);
+				SystemPropertyUtil.getTestDataSheetPath());
 		// sheetCollection = excelReader.GetSheetCollection();
 		for (int i = 0; i < tableData.size(); i++) {
 			String sheetName = sheetCollection.get(i);
