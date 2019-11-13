e7c2a119b87d78c8e0e7eaf25df3fcdb52ea4869
Rupeshkv <Venkatrupesh@gmail.com>
2019-11-13 06:24:46 +0000

    Updated Extent reports

--- src/test/java/com/test/automation/common/framework/ExtentReporter.java
+++ src/test/java/com/test/automation/common/framework/ExtentReporter.java
@@ -3,6 +3,8 @@ package com.test.automation.common.framework;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
@@ -13,62 +15,85 @@ public class ExtentReporter {
 
 	private ExtentReports extent;
 	private ExtentTest test;
-	
+	private String reportPath = null;
+
 	public ExtentReporter() {
 		try {
 			String homepath = new File(".").getCanonicalPath();
-			extent = new ExtentReports(homepath + "\\Automation_Report\\" + "Run_" + Util.getCurrentDate() + "_"
-					+ Util.getCurrentTime() + "\\ReportSummary.html");
+//			reportPath = homepath + "\\Automation_Report\\" + "Run_" + Util.getCurrentDate() + "_"
+//					+ Util.getCurrentTime() + "\\ReportSummary.html";
+
+			reportPath = homepath + "\\Automation_Report\\" + "Run_" + Util.getCurrentDate() + "_"
+					+ Util.getCurrentTime();
+			extent = new ExtentReports(reportPath + "\\ReportSummary.html");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void startTest(String testClass, String testMethod, SeHelper se) {
 		test = extent.startTest((testClass + " :: " + testMethod), testMethod);
 		test.assignAuthor("VAM QA");
 		test.assignCategory(testMethod);
-		test.log(LogStatus.INFO, "Started Execution", "URL: " + SystemPropertyUtil.getBaseStoreUrl() + ":: Browser: " + se.browser().getBrowserName());
+		test.log(LogStatus.INFO, "Started Execution",
+				"URL: " + SystemPropertyUtil.getBaseStoreUrl() + ":: Browser: " + se.browser().getBrowserName());
 	}
-	
+
 	public void reportInfo(String step, String details) {
 		test.log(LogStatus.INFO, step, details);
 	}
-	
+
 	public void reportPass(String step, String details) {
 		test.log(LogStatus.PASS, step, details);
 	}
-	
+
 	public void reportFail(String step, String details) {
 		test.log(LogStatus.FAIL, step, details);
 	}
-	
+
 	public void reportFailCapture(String step, String details, String captureName, SeHelper se) {
 		try {
-			test.log(LogStatus.FAIL, step, details + test.addScreenCapture(Util.captureScreenshot(Util.getCurrentDate() + "_" + captureName, se)));
+			test.log(LogStatus.FAIL, step, details
+					+ test.addScreenCapture(Util.captureScreenshot(Util.getCurrentDate() + "_" + captureName, se)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void reportError(String step, String details) {
 		test.log(LogStatus.ERROR, step, details);
 	}
-	
+
 	public void reportErrorCapture(String step, String details, String captureName, SeHelper se) {
 		try {
-			test.log(LogStatus.ERROR, step, details + test.addScreenCapture(Util.captureScreenshot(Util.getCurrentDate() + "_" + captureName, se)));
+			test.log(LogStatus.ERROR, step, details
+					+ test.addScreenCapture(Util.captureScreenshot(Util.getCurrentDate() + "_" + captureName, se)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void endTest() {
 		extent.endTest(test);
+		extent.getReportId();
+		
 	}
-	
+
 	public void closeExtent() {
 		extent.flush();
 		extent.close();
+		copyfile(reportPath);
+	}
+	
+	private void copyfile(String reportPath2) {
+		File source = new File(reportPath2);
+		File dest = new File(
+				"C:\\Users\\VMQApractice\\Desktop\\VAMQAPractice\\VM_Framework_Base\\VM_Framework_Base\\Automation_Report\\Report\\");
+		try {
+			FileUtils.copyDirectory(source, dest);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
