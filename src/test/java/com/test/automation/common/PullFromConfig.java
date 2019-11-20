bc1252c7fdd63312774c9d9f3a81e00de070cb2f
Rupeshkv <venkatrupesh@gmail.com>
2019-11-20 06:55:20 +0000

    Code for config.properties direct path

--- src/test/java/com/test/automation/common/PullFromConfig.java
+++ src/test/java/com/test/automation/common/PullFromConfig.java
@@ -1,6 +1,7 @@
 package com.test.automation.common;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,31 +12,27 @@ public class PullFromConfig {
 	InputStream inputStream;
  
 	public PullFromConfig() {
- 
-		try {
-			//String canon = new File(".").getCanonicalPath();
-			String propFileName = "config.properties";
- 
-			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
- 
-			if (inputStream != null) {
-				prop.load(inputStream);
-			} else {
-				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-			}
-			System.out.println(prop.getProperty("BaseURL"));
-			System.out.println(prop.getProperty("Browser"));
- 
-		} catch (Exception e) {
-			System.out.println("Exception: " + e);
-		} finally {
-			try {
-			inputStream.close();
-			} catch (IOException e) {
-				System.out.println("inputStream failed to close");
-			}
-		}
-	}
+
+        try(InputStream inputStream = new FileInputStream(SystemPropertyUtil.getRootPath() + "\\resources\\config.properties")) {
+            //String canon = new File(".").getCanonicalPath();
+           
+            //inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+                   
+
+            prop.load(inputStream);
+            System.out.println(prop.getProperty("BaseURL"));
+            System.out.println(prop.getProperty("Browser"));
+
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        } //finally {
+//            try {
+//            inputStream.close();
+//            } catch (IOException e) {
+//                System.out.println("inputStream failed to close");
+//            }
+//        }
+    }
 	
 	public Properties getConfigProp() {
 		return this.prop;
