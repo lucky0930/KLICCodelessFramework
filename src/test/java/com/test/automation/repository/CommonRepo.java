200fa9dc438b2cda18b31cb194f1bfe6b5110113
Rupeshkv <venkatrupesh@gmail.com>
2019-12-26 13:11:00 +0000

    Update for the Task 96

--- src/test/java/com/test/automation/repository/CommonRepo.java
+++ src/test/java/com/test/automation/repository/CommonRepo.java
@@ -14,35 +14,114 @@ public class CommonRepo {
 		return se.element().getElement(username);
 	}
 
-	public static WebElement ElementObject(SeHelper se, String xPathExpression) 
-	{
+	public static WebElement ElementObject(SeHelper se, String expression) {
 		long startWait = System.nanoTime();
-		
-		
-		WebElement element = getElement(se, xPathExpression);
-		
-		long stopWait = System.nanoTime();
-		
-		long implicitWait = ((stopWait - startWait) / 1000000);
-		
-		System.out.print("\n IMPLICIT WAIT TIME " + implicitWait + " MS \n");
 
-		if (element == null) {
-			se.waits().waitForElement(By.xpath(xPathExpression));
+		
+		String typeOfExpression = getTypeOfExpression(expression);
+//		
+//		
+//		if(typeOfExpression != null)
+//		{
+//			typeOfExpression = expression.substring(expression.indexOf('=') + 1);
+//		}
+		
+
+		if (expression.startsWith("/")) {
+
+			WebElement element = getXPathElement(se, expression);
+
+			long stopWait = System.nanoTime();
+
+			long implicitWait = ((stopWait - startWait) / 1000000);
+
+			System.out.print("\n IMPLICIT WAIT TIME " + implicitWait + " MS \n");
+
+			if (element == null) {
+				se.waits().waitForElement(By.xpath(expression));
+			}
+
+			if (!element.isDisplayed()) {
+				se.waits().waitForElementIsDisplayed(By.xpath(expression));
+			}
+
+			return getXPathElement(se, expression);
+		}
+		else if(typeOfExpression == null)
+		{
+			WebElement element = getCSSElement(se, expression);
+
+			long stopWait = System.nanoTime();
+
+			long implicitWait = ((stopWait - startWait) / 1000000);
+
+			System.out.print("\n IMPLICIT WAIT TIME " + implicitWait + " MS \n");
+
+			if (element == null) {
+				se.waits().waitForElement(By.cssSelector(expression));
+			}
+
+			if (!element.isDisplayed()) {
+				se.waits().waitForElementIsDisplayed(By.cssSelector(expression));
+			}
+
+			return getCSSElement(se, expression);
+		}
+		else
+		{
+			
+			String newExpression = expression.substring(expression.indexOf('=') + 1);
+			
+			return getElementBasedOnExpression(se, newExpression, typeOfExpression);
 		}
 		
-		
-		if(!element.isDisplayed()) {
-			se.waits().waitForElementIsDisplayed(By.xpath(xPathExpression));
-		}
-		
-		return getElement(se, xPathExpression);
 		// return se.element().getElement(By.xpath(xPathExpression));
 	}
 
-	private static WebElement getElement(SeHelper se, String xPathExpression) {
+	private static WebElement getElementBasedOnExpression(SeHelper se, String newExpression, String typeOfExpression) {
+		switch(typeOfExpression)
+		{
+			case "css":
+				WebElement element = getCSSElement(se, newExpression);
+
+				long stopWait = System.nanoTime();
+
+				//long implicitWait = ((stopWait - startWait) / 1000000);
+
+				//System.out.print("\n IMPLICIT WAIT TIME " + implicitWait + " MS \n");
+
+				if (element == null) {
+					se.waits().waitForElement(By.cssSelector(newExpression));
+				}
+
+				if (!element.isDisplayed()) {
+					se.waits().waitForElementIsDisplayed(By.cssSelector(newExpression));
+				}
+
+				return getCSSElement(se, newExpression);
+		}
+		return null;
+		
+	}
+
+	private static String getTypeOfExpression(String expression) {
+		String[] selectors = { "Xpath", "CSS", "id", "name", "Linktext", "PartialLinktext", "TagName", "Classname" };
+
+		for (int i = 0; i <= selectors.length; i++) {
+			if (expression.startsWith(selectors[i]))
+				return selectors[i];
+		}
+		return null;
+	}
+
+	private static WebElement getXPathElement(SeHelper se, String xPathExpression) {
 
 		return se.element().getElement(By.xpath(xPathExpression));
+	}
+
+	private static WebElement getCSSElement(SeHelper se, String cssExpression) {
+
+		return se.element().getElement(By.cssSelector(cssExpression));
 	}
 
 }
