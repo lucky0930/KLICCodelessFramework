1fbf9c23103ecadebc213cd5351698fa4b452329
Robert Burke <robert.burke@valuemomentum.com>
2020-02-10 15:55:04 -0500

    Committing progress towards parsing PDF documents in webviewer

--- src/test/java/com/test/automation/common/Utils/PDFReader.java
+++ src/test/java/com/test/automation/common/Utils/PDFReader.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import junit.framework.Assert;
 
 import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessRead;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -34,11 +35,57 @@ public class PDFReader {
 		
 	 // To verify PDF content in the pdf document
 	
-	public void verifyPDFTextInBrowser(String reqTextInPDF) {
+public boolean verifyPDFInBrowser(String reqTextInPDF) {
 		
-		driver.get("http://www.princexml.com/samples/");
-		driver.findElement(By.linkText("PDF flyer")).click();
-		Assert.assertTrue(verifyPDFContent(driver.getCurrentUrl(), reqTextInPDF));
+		boolean flag = false;
+		
+		PDFTextStripper pdfStripper = null;
+		PDDocument pdDoc = null;
+		COSDocument cosDoc = null;
+		String parsedText = null;
+
+		String[] windowsArray = (String[]) driver.getWindowHandles().toArray();
+		driver.switchTo().window(windowsArray[1]);
+		
+		try {
+			URL url = new URL(driver.getCurrentUrl());
+			BufferedInputStream file = new BufferedInputStream(url.openStream());
+			PDFParser parser = new PDFParser((RandomAccessRead) file);
+			
+			parser.parse();
+			cosDoc = parser.getDocument();
+			pdfStripper = new PDFTextStripper();
+			pdfStripper.setStartPage(1);
+			pdfStripper.setEndPage(1);
+			
+			pdDoc = new PDDocument(cosDoc);
+			parsedText = pdfStripper.getText(pdDoc);
+		} catch (MalformedURLException e2) {
+			System.err.println("URL string could not be parsed "+e2.getMessage());
+		} catch (IOException e) {
+			System.err.println("Unable to open PDF Parser. " + e.getMessage());
+			try {
+				if (cosDoc != null)
+					cosDoc.close();
+				if (pdDoc != null)
+					pdDoc.close();
+			} catch (Exception e1) {
+				e.printStackTrace();
+			}
+			driver.close();
+			if (!driver.getWindowHandle().contentEquals(windowsArray[0]))
+			driver.switchTo().window(windowsArray[0]);
+		}
+		
+		System.out.println("+++++++++++++++++");
+		System.out.println(parsedText);
+		System.out.println("+++++++++++++++++");
+
+		if(parsedText.contains(reqTextInPDF)) {
+			flag=true;
+		}
+		
+		return flag;
 	}
 
 /*	
@@ -54,7 +101,7 @@ public class PDFReader {
 	
 	public boolean verifyPDFContent(String reqTextInPDF) {
 		if (filepath == null) {
-			verifyPDFTextInBrowser(reqTextInPDF);
+			verifyPDFInBrowser(reqTextInPDF);
 			return true;
 		}
 		return verifyPDFContent(filepath, reqTextInPDF);
